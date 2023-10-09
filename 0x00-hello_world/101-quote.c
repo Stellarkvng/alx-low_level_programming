b161f2275e5024c4e8c8b5fc72befddeb1f800c0
Stellarkvng <stellarkvng@gmail.com>
2023-10-08 19:01:08 -0700

    I created the file 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - simple function to print to stderr
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
