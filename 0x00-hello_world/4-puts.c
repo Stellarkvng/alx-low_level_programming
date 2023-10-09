67448afc163f572a692bfaa4299119edc24a03d2
Stellarkvng <stellarkvng@gmail.com>
2023-10-08 17:07:00 -0700

    I created the file 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - function to print with puts
+ *
+ * Return: 0
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return 0;
+}
