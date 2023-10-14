f063649d4a9df331396045c11a195f8eb4332f39
Stellarkvng <stellarkvng@gmail.com>
2023-10-13 18:14:25 -0700

    I created the file 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - function to check if a variable is a base 10 digit 0-9
+ *
+ * @c: variable to be checked
+ *
+ * Return: 1 if digit, otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	return(c >= '0' && c <= '9');
+}
