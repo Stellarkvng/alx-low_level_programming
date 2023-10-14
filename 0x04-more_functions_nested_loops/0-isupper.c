4469c2b2021031cc1fa9a890d2127d20d488c6c5
Stellarkvng <stellarkvng@gmail.com>
2023-10-13 18:04:11 -0700

    I created three directories and multiple files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - function to check if a variable is uppercase
+ *
+ * @c: variable to be checked
+ *
+ * Return: 1 if uppercase, otherwise 0
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _isupper - function that checks if a variable is uppercase
+ *
+ * @c: variable to be checked
+ *
+ * Return: 1 if uppercase, otherwise 0
+ */
+
+int _isupper(int c);
+
+#endif /* MAIN_H */
