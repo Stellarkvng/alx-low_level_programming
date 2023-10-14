470aac81f94f22a2b5f38359e140f01943054dc2
Stellarkvng <stellarkvng@gmail.com>
2023-10-13 18:29:39 -0700

    I created a new file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two variables
+ *
+ * @a: variable to be multiplied
+ *
+ * @b: variable to be multiplied
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+	;
