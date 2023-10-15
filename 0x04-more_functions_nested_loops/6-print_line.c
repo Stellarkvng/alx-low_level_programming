027485b4167a4b34385b80d392d4f594cda61a09
Stellarkvng <stellarkvng@gmail.com>
2023-10-14 17:04:56 -0700

    I created the file 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - function to draw a straight line with '_'
+ *
+ * @n: number of times to print '_'
+ */
+
+void print_line(int n)
+{
+	int c;
+
+	for (c = 0; c > n; c++)
+	{
+		if (n > 0)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
